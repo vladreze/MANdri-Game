edc1a81b09d0fad6803236be1d3bc9e2b35d341f
Bordachenko Sofiia <s.bordachenko@gmail.com>
2026-06-05 20:51:18 +0300

    feat: create music and image managers
    Signed-off-by: Bordachenko Sofiia <s.bordachenko@gmail.com>

--- core/src/main/java/com/mandri/game/Main.java
+++ core/src/main/java/com/mandri/game/Main.java
@@ -1,11 +1,31 @@
 package com.mandri.game;
 
 import com.badlogic.gdx.Game;
+import com.mandri.storage.MainAssetsManager;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
+    private MainAssetsManager manager;
     @Override
     public void create() {
         setScreen(new FirstScreen());
+        manager = new MainAssetsManager();
+        manager.loadResources();
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        manager.disposeAll();
     }
 }
--- core/src/main/java/com/mandri/storage/ImageManager.java
+++ core/src/main/java/com/mandri/storage/ImageManager.java
@@ -1,4 +1,64 @@
 package com.mandri.storage;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 public class ImageManager {
+    private final AssetManager manager;
+
+    public ImageManager(AssetManager manager) {
+        this.manager = manager;
+    }
+    SpriteBatch batch;
+    public Animation<TextureRegion> spaceIdle_F;
+    public Animation<TextureRegion> spaceIdle_L;
+    public Animation<TextureRegion> spaceIdle_R;
+
+    public Animation<TextureRegion> forestIdle_F;
+    public Animation<TextureRegion> forestIdle_L;
+    public Animation<TextureRegion> forestIdle_R;
+
+    public Animation<TextureRegion> caveIdle_F;
+    public Animation<TextureRegion> caveIdle_R;
+    public Animation<TextureRegion> caveIdle_L;
+
+    public Animation<TextureRegion> spaceRun_R;
+    public Animation<TextureRegion> spaceRun_L;
+
+    public Animation<TextureRegion> forestRun_R;
+    public Animation<TextureRegion> forestRun_L;
+
+    public Animation<TextureRegion> caveRun_R;
+    public Animation<TextureRegion> caveRun_L;
+
+    public void loadImages(){
+//        manager.load("menu_bg.png", Texture.class);
+//        manager.load("space_bg.png", Texture.class);
+//        manager.load("forest_bg.png", Texture.class);
+//        manager.load("cave_bg.png", Texture.class);
+
+        manager.load("space/space_front.png", Texture.class);
+        manager.load("space/space_left.png", Texture.class);
+        manager.load("space/space_right.png", Texture.class);
+        manager.load("space/space_fall_F.png", Texture.class);
+        manager.load("space/space_fall_L.png", Texture.class);
+        manager.load("space/space_fall_R.png", Texture.class);
+        manager.load("space/space_jump_F.png", Texture.class);
+        manager.load("space/space_jump_L.png", Texture.class);
+        manager.load("space/space_jump_R.png", Texture.class);
+        manager.load("space/space_run_L.png", Texture.class);
+        manager.load("space/space_run_R.png", Texture.class);
+
+        manager.finishLoading();
+
+    }
+
+    public void disposeImages(){
+        manager.dispose();
+    }
+
 }
