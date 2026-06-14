34ae24ee753f8fb7fa2c2c1da56e692a26b75392
dariaradko5-dev <dariaradko5@gmail.com>
2026-06-14 19:13:25 +0300

    feat: add forest map

--- assets/maps/forestMap/forest.tsx
+++ assets/maps/forestMap/forest.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="forest" tilewidth="16" tileheight="16" tilecount="32" columns="8">
- <image source="forestBlocks.png" width="128" height="64"/>
+<tileset version="1.10" tiledversion="1.12.2" name="forest" tilewidth="16" tileheight="16" tilecount="25" columns="5">
+ <image source="forestBlocks.png" width="80" height="80"/>
 </tileset>
