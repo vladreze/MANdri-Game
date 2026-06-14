557fdc0b98e7a45ee82769915bd4665a0ff8a967
Ivan <bozhenko.iivan@gmail.com>
2026-06-14 12:23:44 +0300

    add: partciles

--- core/src/main/java/com/mandri/entities/InventoryLogic.java
+++ core/src/main/java/com/mandri/entities/InventoryLogic.java
@@ -3,8 +3,11 @@ package com.mandri.entities;
 public class InventoryLogic {
     Item[] slots = new Item[16];
 
+    private int activeRow = 0;
+    private int selectedCol = 0;
+
     public InventoryLogic() {
-        this.slots = slots;
+
     }
 
     public boolean addItem(Item item) {
@@ -19,5 +22,50 @@ public class InventoryLogic {
 
     public Item[] getSlots() {
         return slots;
+    }
+
+    public void selectColumn(int col) {
+        if (col >= 0 && col < 4) {
+            selectedCol = col;
+        }
+    }
+
+    public void nextRow() {
+        activeRow++;
+        if (activeRow > 3) {
+            activeRow = 0;
+        }
+    }
+
+    public void prevRow() {
+        activeRow--;
+        if (activeRow < 0) {
+            activeRow = 3;
+        }
+    }
+
+    public int getGlobalSelectedIndex() {
+        return (activeRow * 4) + selectedCol;
+    }
+
+    public int getActiveRow() {
+        return activeRow;
+    }
+
+    public int getSelectedCol() {
+        return selectedCol;
+    }
+
+    public Item getSelectedItem() {
+        return slots[getGlobalSelectedIndex()];
+    }
+
+    public boolean hasItem(String itemName) {
+        for (int i = 0; i < slots.length; i++) {
+            if (slots[i] != null && slots[i].getName().equals(itemName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
