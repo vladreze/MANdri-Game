7ea2b38cb4d30bd5f46b68a100a9199ce8a835c4
vladreze <vladrezanov07@gmail.com>
2026-06-14 10:16:26 +0300

    feat: refactor game over fade animation method for improved reusability

--- core/src/main/java/com/mandri/ui/AnimationActions.java
+++ core/src/main/java/com/mandri/ui/AnimationActions.java
@@ -1,22 +1,21 @@
 package com.mandri.ui;
 
-import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 public class AnimationActions {
 
-    public static void gameOverFade(Table heartTable, Table gameOverTable){
-        heartTable.addAction(Actions.sequence(
-            Actions.fadeIn(0.6f),     // Плавно з'являється за 0.6 сек
-            Actions.delay(1.0f),      // Зависає на екрані на 1 секунду
+    public static void fadeAnimation(Table tableOne, Table tableTwo){
+        tableOne.addAction(Actions.sequence(
+            Actions.fadeIn(0.6f),
+            Actions.delay(2.0f),
             Actions.fadeOut(0.6f),
             Actions.run(new Runnable() {
                 @Override
                 public void run() {
-                    heartTable.setVisible(false);
-                    gameOverTable.setVisible(true);
-                    gameOverTable.addAction(Actions.fadeIn(0.8f));
+                    tableOne.setVisible(false);
+                    tableTwo.setVisible(true);
+                    tableTwo.addAction(Actions.fadeIn(0.8f));
                 }
             })
         ));
